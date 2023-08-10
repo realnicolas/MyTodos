601dda9516d424a51440d14f46ad313bbfeb82ec
realnicolas <nicolasmartinez01@yahoo.com.br>
2023-08-10 12:56:26 -0300

    deleting files

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,5 @@
-import adapter from 'svelte-adapter-github';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
-
-const dev = process.env.NODE_ENV === 'development';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -13,20 +11,7 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			pages: 'docs',
-			assets: 'docs',
-			fallback: null,
-			precompress: false,
-			domain: '',
-			jekyll: false
-		}),
-		paths: {
-			base: dev ? '' : '/my-todos'
-		},
-		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
-		// For example, instead of '_app', use 'app_', 'internal', etc.
-		appDir: 'internal'
+		adapter: adapter(),
 	}
 };
 
