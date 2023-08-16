2834f74564a5703a4e0fa1ee73b60fb50897075d
Nicolas Martinez <nicolasmartinez01@yahoo.com.br>
2023-08-16 17:02:58 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import svg from '@poppanator/sveltekit-svg';
 
 export default defineConfig({
-	plugins: [sveltekit(), svg()]
+	plugins: [sveltekit()]
 });
