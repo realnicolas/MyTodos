8db090f4a9836f38ab6f78051a6f303e5a5dadd5
Nicolas Martinez <nicolasmartinez01@yahoo.com.br>
2023-10-17 14:45:17 +0000

    Structure changes

---
+++ src/lib/components/AddTodo.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import type { TodoType } from '$lib/types/TodoType';
+	import type { Writable } from 'svelte/store';
+	export let myTodos: Writable<TodoType[]>;
+	let inputValue: string;
+
+	function addTodo() {
+		if (!inputValue.trim()) return;
+		$myTodos = [
+			...$myTodos,
+			{ id: crypto.randomUUID(), text: inputValue.trim(), completed: false }
+		];
+		inputValue = '';
+	}
+</script>
+
+<div class="py-6 flex flex-col gap-2 md:flex-row md:gap-4">
+	<input
+		type="text"
+		bind:value={inputValue}
+		class="border border-black dark:border-white bg-transparent rounded p-1 flex-1"
+	/>
+
+	<button on:click={addTodo} class="btn bg-primary-500 py-1 text-on-primary-token md:w-[5rem]">
+		Add
+	</button>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
-	import AddTodoForm from '$lib/components/AddTodoForm.svelte';
-	import TodoItem from '$lib/components/TodoItem.svelte';
+	import AddTodo from '$lib/components/AddTodo.svelte';
+	import TodoList from '$lib/components/TodoList.svelte';
 	import { myTodos } from '$lib/stores';
-	import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
-	$: numberOfTodosCompleted = $myTodos.filter((todo) => todo.completed === true).length;
 </script>
 
 <div class="mx-auto max-w-[600px] [&>*]:mx-4">
-	<AddTodoForm />
+	<AddTodo {myTodos} />
 
 	{#if $myTodos.length === 0}
 		<div class="text-center text-stone-700 dark:text-stone-300">
@@ -15,30 +13,6 @@
 			<h1>Enjoy your day!!!</h1>
 		</div>
 	{:else}
-		<div class="">
-			<ul class="divide-y">
-				{#each $myTodos as todo (todo.id)}
-					{#if todo.completed === false}
-						<TodoItem {...todo} />
-					{/if}
-				{/each}
-			</ul>
-			<Accordion class="bg-black/20 rounded mt-1" padding='py-2 px-1' regionControl="hover:bg-tertiary-hover-token" regionPanel="py-0">
-				<AccordionItem class="focus:bg-none">
-					<svelte:fragment slot="summary">
-						Completed ({numberOfTodosCompleted})
-					</svelte:fragment>
-					<svelte:fragment slot="content">
-						<ul class="divide-y">
-							{#each $myTodos as todo (todo.id)}
-								{#if todo.completed === true}
-									<TodoItem {...todo} />
-								{/if}
-							{/each}
-						</ul>
-					</svelte:fragment>
-				</AccordionItem>
-			</Accordion>
-		</div>
+		<TodoList {myTodos} />
 	{/if}
 </div>
